d2b6d3e9b68285d685fc6a7f893d6733ac09ae44
Victor Umbelino Barbosa <geraldopp755@gmail.com>
2025-08-11 17:48:28 -0300

    Add files via upload

---
+++ sport-bar-e-salao-de-festas.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import os
+import tempfile
+from datetime import datetime
+def imagem_ao_fundo():
+    st.markdown(
+        <style>
+        .stApp{{
+            background-image:url(https://delman.com.br/admin/wp-content/uploads/2020/12/IMG_0956-Copia-1024x683.jpg);
+            background-attachment: fixed;
+            background-size: cover;
+        }}
+        <style>
+
+        unsafe_allow_html=True
+    )
+imagem_ao_fundo()
+st.title("Reserva do Sport Bar e do Salão de Festas")
